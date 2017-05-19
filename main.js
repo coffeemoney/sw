b915e931ea783e2b15194a83607d711b36e44d42
coffeemoney <coffeemoney@gmail.com>
2017-05-20 01:20:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,4 +12,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
   res.end();
-}).listen(9000);
+}).listen(8080);
